422f44625f84e7b7987b6ac2552ca9318629e631
Michael Collis <mikec@whoisstudio.com>
2015-02-27 15:23:55 -0500

    creating dist

--- src/app/contact/contact.controller.js
+++ src/app/contact/contact.controller.js
@@ -3,14 +3,12 @@
 
   function ContactCtrl( Mandrill ) {
     var vm = this;
-
     function sendEmail( contact ) {
       Mandrill.send( contact ).then( function( data ) {
         vm.contact = {};
         vm.message = 'Message Sent';
       } );
     }
-
     vm.sendEmail = sendEmail;
   }
 
